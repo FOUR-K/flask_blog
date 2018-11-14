01e8b9666914858ab06b6912d813e2c4ce116198
fourK <innocenthacker84@gmail.com>
2018-11-14 13:55:20 +0530

    added os DB

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -2,7 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = '4ba307f9992d3170d84ad354e7f5183b'
-    SQLALCHEMY_DATABASE_URI= 'sqlite:///site.db'
+    SQLALCHEMY_DATABASE_URI= os.environ.get('DATABASE_URL') or \
+        'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
